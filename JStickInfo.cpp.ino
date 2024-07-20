e0383a1c17ea21c95f612fdcd0e6060f30d326bb
Abhijun Gupta <54921281+Abhijun@users.noreply.github.com>
2024-07-20 14:39:33 -0700

    Add files via upload

---
+++ JStickInfo.cpp.ino
@@ -0,0 +1,15 @@
+
+class Jstick_Info
+{
+
+
+
+
+
+  uint8_t lstickx;
+  uint8_t lsticky;
+  uint8_t rstickx;
+  uint8_t rsticky;
+  uint8_t ltrigger;
+  uint8_t rtrigger;
+}
---
+++ udp_connecter.cpp.ino
@@ -0,0 +1,13 @@
+import JStickInfo.h
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
