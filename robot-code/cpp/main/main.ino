bf28ec5b75624b29f0031c0336778e2efa7717f2
pogace <elispogace@sfhs.com>
2024-07-22 01:18:37 -0700

    where the arduino code goes

---
+++ robot-code/cpp/main/main.ino
@@ -0,0 +1,12 @@
+#include "../CommsManager.h"
+#include "../JStickInfo.h"
+CommsManager my_manager("test","test2","robotname");
+
+void setup() {
+  my_manager.Initialize();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
